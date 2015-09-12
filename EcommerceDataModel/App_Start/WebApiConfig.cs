b3cd80e644a5c639093109fbfca2767157eecb43
Mahendran <mahendu_msc@yahoo.co.in>
2015-09-12 22:30:01 +0530

    ExtensibleDataModelAndRest

---
+++ EcommerceDataModel/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace EcommerceDataModel
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            
+            config.Routes.MapHttpRoute(
+                name: "customerquery",
+                routeTemplate: "api/{controller}/{action}/{query}",
+                defaults: new { query = RouteParameter.Optional }
+            );
+            
+            config.Routes.MapHttpRoute(
+               name: "byfilter",
+               routeTemplate: "api/{controller}/{action}/{id}/{filter}",
+               defaults: new { id = RouteParameter.Optional, filter = RouteParameter.Optional }
+           );
+
+            
+        }
+    }
+}
